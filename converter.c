25646cf99ecf41f785f35118bdc3f40413803d88
matxa <matxa21@gmail.com>
2019-10-28 23:19:01 +0000

    conv

---
+++ converter.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * convert - converter
+ * @n - number
+ * @base: base
+ * Return: pointer
+ */
+
+char *convert(unsigned int num, int base)
+{
+	char *hex = "0123456789ABCDEF";
+	char buffer[50];
+	char *ptr;
+
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do
+	{
+		*--ptr = hex[num%base];
+		num /= base;
+	}while (num != 0);
+
+	return (ptr);
+}
+
+int main()
+{
+	unsigned int i = 432;
+	puts(convert(i, 8));
+	return 0;
+}
