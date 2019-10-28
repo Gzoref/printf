312e25de1dd03db385c3bce70a03acf761db49d1
Gzoref <gzoref@gmail.com>
2019-10-27 21:59:10 -0400

    1st commit with fucntion templates

---
+++ format_functions.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+* func - function description
+*
+* param1
+*
+* param2
+*
+* Return: return type
+*/
+
+int get_int(va_list arg)
+{
+
+}
+
+
+/**
+* func - function description
+*
+* param1
+*
+* param2
+*
+* Return: return type
+*/
+
+int get_char(va_list arg)
+{
+
+}
+
+
+/**
+* func - function description
+*
+* param1
+*
+* param2
+*
+* Return: return type
+*/
+
+int get_string(va_list arg)
+{
+
+}
