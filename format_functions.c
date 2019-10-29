d3e2e3f8d9e4a63652f1326166ef437c30649840
Gzoref <gzoref@gmail.com>
2019-10-28 23:59:51 -0400

    v 0.5

--- format_functions.c
+++ format_functions.c
@@ -12,68 +12,51 @@
 
 int get_int(va_list arg)
 {
-	int num1, num2, length, digit, flag;
-	char buffer [2] = { '0', '\0'};
+	int num1, num2, index, digit, last_digit, exponent;
 
 	num1 = va_arg(arg, int);
+	exponent = 1;
+	index = 1;
+	last_digit = num1 % 10;
 
-	length = 1000000000;
-	num2 = 0;
-	flag = 0;
+	num1 /= 10;
+	num2 = num1;
 
 
 	/*  Negative sign  */
-	if (num1 < 0)
+	if (last_digit < 0)
 	{
-		write(1, "-", 1);
+		_putchar('-');
 
 		/*  Multiply by -1 to make number negative  */
 		num1 *= -1;
+		last_digit *= -1;
+		index++;
 	}
 
 
 	/*  If num is zero, print 0 */
-	if (num1 == 0)
-	{
-		write(1, "0", 1);
-		return (1);
-	}
-
-		/*  If num equals type int minimum  */
-	if (num1 == INT_MIN)
-	{
-		flag = 1;
-		num1++;
-	}
-
-	/* Handles positive integers */
 	if (num1 > 0)
 	{
-		while (num1 / length < 0)
+		while (num2 / 10 != 0)
 		{
-			length /= 10;
+			exponent *= 10;
+			num2 /= 10;
 		}
-		while (num1 > 0)
-		{
-			num2++;
-			digit = num1 / length;
-			length /= 10;
-			buffer[0] = digit + '0';
+		num2 = num1;
 
-			if (flag == 1)
-			{
-				num1++;
-			}
-			flag = 0;
-			write(1, buffer, 1);
-		}
-		if (num1 == 0 && length == 1)
+		while (exponent > 0)
 		{
-			num2++;
-			write(1, "0", 1);
+			digit = num2 / exponent;
+			_putchar(digit + '0');
+			num2 = num2 - (digit * exponent);
+			exponent /= 1;
+			index++;
 		}
 	}
-	return (num2);
+	_putchar(last_digit + '0');
+
+	return (index);
 }
 
 
@@ -92,7 +75,7 @@ int get_char(va_list arg)
 	char character;
 
 	character = va_arg(arg, int);
-	write(1, &character, 1);
+	_putchar(character);
 
 	return (1);
 }
