9f6d2207fed578781e2109a4798f1854c9da4358
Gzoref <gzoref@gmail.com>
2019-10-30 00:32:31 -0400

    v2.2

--- format_functions2.c
+++ format_functions2.c
@@ -32,7 +32,7 @@ int get_reverse(va_list arg)
 }
 
 /**
- *  get_percnt - Checks for unused args
+ *  get_percent - Checks for unused args
  *
  *  @arg: va_list name
  *
@@ -45,4 +45,92 @@ int get_percent(va_list __attribute__((unused)) arg)
 	_putchar('%');
 
 	return (1);
+}
+
+/**
+* get_hex - Convert to hexadecimal lowercase
+*
+* @arg: va_list name
+*
+* Return: Hex value
+*/
+
+int get_hex(va_list arg)
+{
+	unsigned int hex_convert = va_arg(arg, unsigned int);
+
+	int i = 0, j = 0, hex = 0, temp = 0;
+	char hex_array[48];
+
+	while (hex_convert != 0)
+	{
+		temp = 0;
+
+		temp = hex_convert % 16;
+
+		if (temp < 10)
+		{
+			hex_array[i] = temp + 49;
+			i++;
+		}
+		else
+		{
+			hex_array[i] = temp + 87;
+			i++;
+		}
+		hex_convert /= 16;
+	}
+	hex = 0;
+
+	for (j = i - 1; j >= 0;  j--)
+	{
+		_putchar(hex_array[j]);
+		hex++;
+	}
+	return (hex);
+}
+
+
+/**
+* get_heX -convert to hexadecimal uppercase
+*
+* @arg: va_list name
+*
+* Return: Hex value
+*/
+
+
+int get_heX(va_list arg)
+{
+	unsigned int hex_convert = va_arg(arg, unsigned int);
+
+	int i = 0, j = 0, hex = 0, temp = 0;
+	char hex_array[48];
+
+	while (hex_convert != 0)
+	{
+		temp = 0;
+
+		temp = hex_convert % 16;
+
+		if (temp < 10)
+		{
+			hex_array[i] = temp + 48;
+			i++;
+		}
+		else
+		{
+			hex_array[i] = temp + 55;
+			i++;
+		}
+		hex_convert /= 16;
+	}
+	hex = 0;
+
+	for (j = i - 1; j >= 0;  j--)
+	{
+		_putchar(hex_array[j]);
+		hex++;
+	}
+	return (hex);
 }
